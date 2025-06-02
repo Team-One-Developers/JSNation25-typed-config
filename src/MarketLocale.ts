13ff429303579dd3405c6ca13c674738cfcb98d0
Jannik Sommerfeld <jannik.sommerfeld@icloud.com>
2025-06-02 08:01:08 +0200

    4.2) cleanup MarketLocale

--- src/MarketLocale.ts
+++ src/MarketLocale.ts
@@ -6,20 +6,9 @@ import {
   Seller,
 } from "./config";
 
-// export type MarketLocaleSimple =
-//   | { marketKey: "de"; locale: "de-DE" }
-//   | { marketKey: "ch"; locale: "de-CH" | "en-CH" };
-
 export type MarketLocale<M extends MarketKey = MarketKey> = {
   [K in M]: { marketKey: K; locale: LocaleOfMarket<K> };
 }[M];
-
-// type MarketLocaleMap = {
-//     de: { marketKey: "de"; locale: "de-DE" }
-//     ch: { marketKey: "ch"; locale: "de-CH" | "en-CH" }
-// }
-
-// type MarketLocaleFromMap<M extends MarketKey = MarketKey> = MarketLocaleMap[M]
 
 export function isMarketLocaleForSeller<S extends Seller>(
   marketLocale: MarketLocale,
